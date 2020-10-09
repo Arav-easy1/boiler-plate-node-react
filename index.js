14edf55ff83c1c6e1dd3878015b989a44510f739
EASY_ARAV <wg0418@gmail.com>
2020-10-09 19:53:06 +0900

    #8 install nodemon

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World!~~~~~~~~~~~~~~~~~~~");
 });
 
 app.post("/register", (req, res) => {
