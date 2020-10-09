bbe5320118aa86a4c34a4847eb00fb637f091738
EASY_ARAV <wg0418@gmail.com>
2020-10-09 20:34:18 +0900

    #9 hide secret information

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const { User } = require("./models/User");
 // bodyParser도 넣어줌.
 const bodyParser = require("body-parser");
 
+const config = require("./config/key");
+
 // bodyParser 옵션
 // application/x-www-form-urlencoded 이렇게 생긴 것을 분석해서 가져오는 옵션
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,15 +20,12 @@ app.use(bodyParser.json());
 // connect()안에 mongodb cluster connection안에 있는 소스를 copy해서 붙여 넣어주고, 뒤에 설정사항들을 적어준다(안적으면 에러날 확률이 높아짐)
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://arav-easy1:arav1234@boilerplate.h0gtw.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  ) // 잘 연결이 됐으면 연결됐다고 뜨고 아니면 에러나오게 함.
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }) // 잘 연결이 됐으면 연결됐다고 뜨고 아니면 에러나오게 함.
   .then(() => console.log("MongoDB Connected.."))
   .catch((err) => console.log(err));
 
